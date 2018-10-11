fa253b25470ef9ada8742455f73b881b8b6d37ce
Tonatiuj Sanchez <tonsanzjimz@gmail.com>
2018-10-11 08:32:52 -0500

    Template listo sin rutas y scripts corregidos para su correcta lectura

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
